d30768adee977a26fab34e2260523ceac2fdc36a
viki2013 <viktoriasikharulidze@gmail.com>
2024-11-07 20:47:03 +0400

    Add files via upload

---
+++ day 01/Homework/day 01 homework.py
@@ -0,0 +1,44 @@
+
+
+name = "viktoria"
+
+surname = "sixarulidze"
+
+print(name)
+age = 11
+height= "150.3"
+weight = "44"
+knows_football=False
+is_dumb= False
+
+print(name + " " + surname)
+
+print(name + " " + str(age))
+
+print(name + " " + str(height))
+
+print(name+ " " + str(weight))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
