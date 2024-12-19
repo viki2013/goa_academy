fc08785bd1faec507496438885436b8c3ff0b011
viki2013 <viktoriasikharulidze@gmail.com>
2024-12-19 10:26:57 -0800

    Update 2.py

--- day 20/homework/2.py
+++ day 20/homework/2.py
@@ -1,5 +1,18 @@
+for i in range(10,30):
+    print(i)
 
+for i in range(20,41):
+    print(i)
 
-for i in range(3,50,60):
+for i in range(2,50,60):
+    print(i)
+
+for i in range(10,50):
+    print(i)
+
+for i in range(10,20):
+    print(i)
+
+for i in range(2,30):
     print(i)
     
