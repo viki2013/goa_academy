dc9b723d43ac7eea7ddfe913bcc0260e76efa1b5
viki2013 <viktoriasikharulidze@gmail.com>
2024-12-27 10:38:56 -0800

    Add files via upload

---
+++ day 22/classwork/1.py
@@ -0,0 +1,6 @@
+correct_num = 10
+num1 = int(input("enter your number:"))
+while num1 != correct_num:
+    num1 = int(input("enter your  new number:"))
+print( f"correct number: {num1}")
+
---
+++ day 22/homework/1.py
@@ -0,0 +1,11 @@
+for goa best in range(50):
+      print("goa best")
+
+num1 = 0
+while num1 < 50:
+    print("goa best")
+    num1 = num1 + 1
+
+ 
+  
+
