e92b52fe11e39c2095cf213b4b78d7dcab26f979
viki2013 <viktoriasikharulidze@gmail.com>
2025-01-07 00:32:45 -0800

    Add files via upload

---
+++ day 26/classwork/1.py
@@ -0,0 +1,13 @@
+
+    
+
+number = int(input("შეიტანეთ მთელი რიცხვი: "))
+
+# პირობითი განცხადება
+if number > 0:
+    print("Bigger than 0")
+elif number == 0:
+    print("0")
+else:
+    print("smaller than 0")
+
