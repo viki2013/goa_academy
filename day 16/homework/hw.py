2ce4bdcc34c9e6f4dffe6b61639a05f89586d83c
viki2013 <viktoriasikharulidze@gmail.com>
2025-02-02 03:27:55 -0800

    Update hw.py

---
+++ day 16/homework/hw.py
@@ -0,0 +1,23 @@
+
+a = 10
+b = 20
+c = 30
+x = True
+y = False
+
+print((a < b) and (b < c))  # True
+
+
+print((a > b) or (b < c))   # True
+
+
+print(not (a == b))         # True
+
+
+print((a != b) and (b == c))  # True
+
+
+print((a < c) or (x == y))  
+
+
+print(x and (b > a))  # True
