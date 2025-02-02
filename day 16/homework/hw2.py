17bb2d1daad1ce7d20ad566432feb00a5ec6209e
viki2013 <viktoriasikharulidze@gmail.com>
2025-02-02 03:28:19 -0800

    Update hw2.py

---
+++ day 16/homework/hw2.py
@@ -0,0 +1,7 @@
+print(True and False)  # False
+print(True or False)   # True
+print(False and False) # False
+print(True and True)   # True
+print(False or True)   # True
+print((True or False) and False) # False
+print((False and True) or True)  # True
