9a8e0e243eaab8aa666eda15080d863a62238044
viki2013 <viktoriasikharulidze@gmail.com>
2025-03-23 04:17:20 -0700

    Add files via upload

---
+++ day 46/classwork/1.py
@@ -0,0 +1,10 @@
+# Tuple-ის შექმნა 5 რიცხვით
+numbers = (3, 7, 2, 7, 3)
+
+# მინიმალური და მაქსიმალური მნიშვნელობების დაბეჭდვა
+print( min(numbers))
+print( max(numbers))
+
+# პირველი ელემენტის რაოდენობის დათვლა
+first_element_count = numbers.count(numbers[0])
+print(first_element_count)
