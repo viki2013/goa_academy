e30cd855b49c7a22b9ccef34544e18c83193246f
viki2013 <viktoriasikharulidze@gmail.com>
2025-04-10 00:22:05 +0400

    Update 1.py

---
+++ day 54/classwork/1.py
@@ -0,0 +1,11 @@
+product = {
+    "name":"viki",
+    "surename":"sikharulidze",
+    "location":"didi digomi",
+    "color":"red",
+    "hobi": "swim"
+}
+
+print(product.keys())
+print(product.values())
+print(product.items())
